52d064b67a577cbaf0d355136fbe4ae09f3d1073
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-25 21:36:00 +0900

    UPDATE monolithic_goods.js
    상품등록 기능 추가, 데이터베이스 설정

--- src/monolithic/monolithic_goods.js
+++ src/monolithic/monolithic_goods.js
@@ -1,3 +1,6 @@
+const mysql = require("mysql");
+const conn = require("../config");
+
 exports.onRequest = (res, method, pathname, params, cb) => {
   switch (method) {
     case "POST":
@@ -14,5 +17,30 @@ exports.onRequest = (res, method, pathname, params, cb) => {
       });
     default:
       return process.nextTick(cb, res, null);
+  }
+}
+
+function register(method, pathname, params, cb) {
+  let response = {
+    errorcode: 0,
+    errormessage: "success",
+  };
+  if(params.name == null || params.category == null || params.price == null || params.description == null) {
+    response.errorcode = 1;
+    response.errormessage = "Invalid Parameters";
+    cb(response);
+  } else {
+    const connection = mysql.createConnection(conn);
+    connection.connect();
+    connection.query("INSERT INTO goods(name, category, price, description) VALUES (?, ?, ?, ?)", [
+      params.name, params.category, params.price, params.description
+    ], (error, result, fields) => {
+      if(error) {
+        response.errorcode = 1;
+        response.errormessage = "error";
+      }
+      cb(response);
+    });
+    connection.end();
   }
 }
