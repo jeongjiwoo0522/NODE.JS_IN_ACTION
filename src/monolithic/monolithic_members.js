4823ee8c817d25b5466af70c1687113761df6e78
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-25 21:56:50 +0900

    UPDATE monolithic_member.js
    회원 등록 기능 추가

--- src/monolithic/monolithic_members.js
+++ src/monolithic/monolithic_members.js
@@ -1,3 +1,6 @@
+const mysql = require("mysql");
+const conn = require("../config");
+
 exports.onRequest = (res, method, pathname, params, cb) => {
   switch (method) {
     case "POST":
@@ -14,5 +17,30 @@ exports.onRequest = (res, method, pathname, params, cb) => {
       });
     default:
       return process.nextTick(cb, res, null);
+  }
+}
+
+function register(method, pathname, params, cb) {
+  let response = {
+    key: params.key,
+    errorcode: 0,
+    errormessage: "success",
+  };
+  if(params.username == null || params.password == null) {
+    response.errorcode = 1;
+    response.errormessage = "Invalid Parameter";
+    cb(response);
+  } else {
+    const conncetion = mysql.createConnection(conn);
+    conncetion.connect();
+    conncetion.query(`INSERT INTO members (username, password) 
+    values (${params.username}, password(${params.password}));`, (error, results, fields) => {
+      if(error) {
+        response.errorcode = 1;
+        response.errormessage = error;
+      }
+      cb(response);
+    });
+    conncetion.end();
   }
 }
