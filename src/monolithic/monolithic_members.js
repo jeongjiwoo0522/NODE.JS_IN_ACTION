4c47d2c0781b72b1e75610756db2b9b3a403b7d4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-25 22:03:06 +0900

    UPDATE monolithic_member.js
    회원 인증 기능 추가

--- src/monolithic/monolithic_members.js
+++ src/monolithic/monolithic_members.js
@@ -44,3 +44,30 @@ function register(method, pathname, params, cb) {
     conncetion.end();
   }
 }
+
+function inquiry(method, pathname, params, cb) {
+  let response = {
+    key: params.key,
+    errorcode: 0,
+    errormessage: "success",
+  };
+  if(params.username == null || params.password == null) {
+    response.errorcode = 1;
+    response.errormessage = "Invalid Parameter";
+    cb(response);
+  } else {
+    const connection = mysql.createConnection(conn);
+    connection.connect();
+    connection.query(`SELECT id FROM members WHERE 
+    username = ${params.username} AND password = password(${params.password});`, (error, result, fields) => {
+      if(error || result.length == 0) {
+        response.errorcode = 1;
+        response.errormessage = error ? error : "Invalid password";
+      } else {
+        response.userId = result[0].id;
+      }
+      cb(response);
+    });
+    connection.end();
+  }
+}
