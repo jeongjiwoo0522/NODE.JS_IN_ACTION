cd393378e16ce36f3e9dc1c54a2d5bf6701f2d11
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-25 22:34:41 +0900

    UPDATE monolithic_purchases.js
    구매 기능 추가

--- src/monolithic/monolithic_purchases.js
+++ src/monolithic/monolithic_purchases.js
@@ -1,3 +1,6 @@
+const mysql = require("mysql");
+const conn = require("../config");
+
 exports.onRequest = (res, method, pathname, params, cb) => {
   switch (method) {
     case "POST":
@@ -10,5 +13,32 @@ exports.onRequest = (res, method, pathname, params, cb) => {
       });
     default:
       return process.nextTick(cb, res, null);
+  }
+}
+
+function register(method, pathname, params, cb) {
+  let response = {
+    key: params.key,
+    errorcode: 0,
+    errormessage: "success",
+  };
+
+  if(params.userid == null || params.goodsid == null) {
+    response.errorcode = 1;
+    response.errormessage = "Invalid Parameter";
+    cb(response);
+  } else {
+    const connection = mysql.createConnection(conn);
+    connection.connect();
+    connection.query("INSERT INTO purchases(userid, goodsid) VALUES (?, ?)", [
+      params.userid, params.goodsid
+    ], (error, results, fields) => {
+      if(error) {
+        response.errorcode = 1;
+        response.errormessage = error;
+      } 
+      cb(response);
+    });
+    connection.end();
   }
 }
