de8590dc09dc2783bb5653f93e82e7d6db9ee68d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-26 21:25:42 +0900

    CREATE gate.js
    http 게이트웨이

---
+++ src/microservice/gate.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const url = require("url");
+const qs = require("querystring");
+
+const server = http.createServer((req, res) => {
+  const method = req.method;
+  const uri = url.parse(req.url, true);
+  const pathname = uri.pathname;
+
+  if(method === "POST" || method === "PUT") {
+    let body = "";
+    req.on("data", (data) => {
+      body += data;
+    });
+    req.on("end", () => {
+      let params;
+      if(req.headers["content-type"] === "application/json") {
+        params = JSON.parse(body);
+      } else {
+        params = qs.parse(body);
+      }
+      onRequest(res, method, pathname, params);
+    });
+  } else {
+    onRequest(res, method, pathname, uri.query);
+  }
+}).listen(8000, () => {
+  console.log("listen", server.address());
+});
+
+function onRequest(res, method, pathname, params) {
+
+}
