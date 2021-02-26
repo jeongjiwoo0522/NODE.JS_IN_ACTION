b588e540dc7ac76a36f0c670bded6e287448e32d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-26 20:50:35 +0900

    CREATE microservice_goods.js
    상품 관리 마이크로서비스

---
+++ src/microservice/microservice_goods.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const business = require("../monolithic/monolithic_goods");
+const Server = require("../distributes/server");
+
+class Goods extends Server {
+  constructor() {
+    super("goods", process.argv[2] ? +process.argv[2] : 9010, ["POST/goods", "GET/goods", "DELETE/goods"]);
+    this.connectToDistributor("127.0.0.1", 9000, (data) => {
+      console.log("Distributor Notification", data);
+    });
+  }
+
+  onRead(socket, data) {
+    console.log("onRead", socket.remoteAddress, socket.remotePort, data);
+    business.onRequest(socket, data.method, data.uri, data.params, (s, packet) => {
+      socket.write(JSON.stringify(packet) + "@");
+    });
+  }
+}
+
+new Goods();
