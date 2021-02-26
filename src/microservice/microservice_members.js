5581ebed824783898f6fd16bc308330b6f4aca40
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-26 21:01:25 +0900

    CREATE microservice_members.js
    회원 관리 마이크로서비스

---
+++ src/microservice/microservice_members.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const business = require("../monolithic/monolithic_members");
+const Server = require("../distributes/server");
+
+class Members extends Server {
+  constructor() {
+    super("members", process.argv[2] ? +process.argv[2] : 9020, ["POST/members", "GET/members", "DELETE/members"]);
+    this.connectToDistributor("127.0.0.1", 9000, (data) => {
+      console.log("Distributor Notification", data);
+    });
+  }
+
+  onRead(socket, data) {
+    console.log("onRead", socket.remoteAddress, socket.remotePort, data);
+    business.onRequest(socket, data.method, data.uri, data.params, (s, packet) => {
+      socket.write(JSON.stringify(packet) + "@");
+    });
+  }
+}
+
+new Members();
