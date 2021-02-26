4f208668ffc145181c79a419d2567b7e33580c31
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-26 19:28:54 +0900

    CREATE server.js
    microservice server class 추가

---
+++ src/microservice/server.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const net = require("net");
+const tcpClient = require("./client");
+
+class tcpServer {
+  constructor(name, port, urls) {
+    this.context = { port, name, urls };
+    this.merge = {};
+    this.server = net.createServer((socket) => {
+      this.onCreate(socket);
+      socket.on("error", (exception) => {
+        this.onClose(socket);
+      });
+      socket.on("close", () => {
+        this.onClose(socket);
+      });
+      socket.on("data", (data) => {
+        const key = `${socket.remoteAddress}:${socket.remotePort}`;
+        const sz = this.merge[key] ? this.merge[key] + data.toString() : data.toString();
+        const arr = sz.split("@");
+        for(let n in arr) {
+          if(sz.charAt(sz.length -1) !== "@" && n == arr.length - 1) {
+            this.merge[key] = arr[n];
+            break;
+          } else if(arr[n] == "") {
+            break;
+          } else {
+            this.onRead(socket, JSON.parse(arr[n]));
+          }
+        }
+      });
+    });
+    this.server.on("error", (err) => {
+      console.error(err);
+    });
+    this.server.listen(port, () => {
+      console.log("listen", this.server.address());
+    });
+  }
+
+  onCreate(socket) {
+    console.log("onCreate", socket.remoteAddress, socket.remotePort);
+  }
+
+  onClose(socket) {
+    console.log("onClose", socket.remoteAddress, socket.remotePort);
+  }
+}
+
+module.exports = tcpServer;
