d3f86b3b64e6fc3b6660eec0843eb6344e5b6c18
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-26 20:20:12 +0900

    UPDATE distributor.js
    접속 이벤트 처리

--- src/microservice/distributor.js
+++ src/microservice/distributor.js
@@ -6,6 +6,41 @@ class Distributor extends TcpServer {
   constructor() {
     super("distributor", 9000, ["POST/distributor", "GET/distributor"]);
   }
+
+  onCreaet(socket) {
+    console.log("onCreate", socket.remoteAddress, socket.remotePort);
+    this.sendInfo(socket);
+  }
+
+  onClose(socket) {
+    const key = `${socket.remoteAddress}:${socket.remotePort}`;
+    console.log("onClose", socket.remoteAddress, socket.remotePort);
+    delete map[key];
+    this.sendInfo();
+  }
+
+  write(socket, packet) {
+    socket.write(JSON.stringify(packet) + "@");
+  }
+
+  sendInfo(socket) {
+    const packet = {
+      uri: "/distributes",
+      method: "GET",
+      key: 0,
+      params: [],
+    };
+    for(let n in map) {
+      packet.params.push(map[n].info);
+    }
+    if(socket) {
+      this.write(socket, packet);
+    } else {
+      for(let n in map) {
+        this.write(map[n].socket, packet);
+      }
+    }
+  }
 }
 
 new Distributor();
