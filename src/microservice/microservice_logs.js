b73154ac9cebe6dd75e5c8951a03c561ac143f3e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-01 12:23:18 +0900

    UPDATE mocroservice_logs.js
    파일을 이용한 로그

--- src/microservice/microservice_logs.js
+++ src/microservice/microservice_logs.js
@@ -1,16 +1,24 @@
 "use strict";
 
+const fs = require("fs");
 const cluster = require("cluster");
 const Server = require("../distributes/server");
 
 class Logs extends Server {
   constructor() {
     super("logs", process.argv[2] ? +process.argv[2] : 9040, ["POST/logs"]);
+
+    this.writestream = fs.createWriteStream("./comon.log", { flags: "a" });
+
+    this.connectToDistributor("127.0.0.1", 9000, (data) => {
+      console.log("Distributor Notification", data);
+    });
   }
 
   onRead(socket, data) {
-    const sz = `${new Date().toLocaleString()}\t${socket.remoteAddress}\t${socket.remotePort}\t${JSON.stringify(data)}\n`;
+    const sz = `${new Date().toLocaleString()} ${socket.remoteAddress} ${socket.remotePort}   ${JSON.stringify(data)}\n`;
     console.log(sz);
+    this.writestream.write(sz);
   }
 } 
 
